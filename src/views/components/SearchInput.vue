f79f34081dcb86351f5aa7210771e6b16f378a84
Raiondesu <a.raiondesu@gmail.com>
2018-04-05 01:30:47 +0300

    Well, at least it works. A little.

---
+++ src/views/components/SearchInput.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  form(@submit.prevent="submitLink")
+    vue-suggest(
+      v-model="query"
+      :destyled="true"
+    )
+    button(type="submit" style="display:none")
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import VueSuggest from 'vue-simple-suggest/lib'
+
+  export default Vue.extend({
+    components: {
+      VueSuggest
+    },
+    data() { return {
+      query: ''
+    }},
+    methods: {
+      submitLink() {
+        location.replace('https://google.com/search?q=' + this['query']);
+      }
+    }
+  })
+</script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,38 +1,18 @@
 <template lang="pug">
-  h1
-    | H{{ sum }}LLO, cruel world!
-    hr
+  search-input
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import SearchInput from './components/SearchInput.vue'
 
   export default Vue.extend({
-    data: () => ({
-      a: 2,
-      b: 1
-    }),
-    computed: {
-      sum(): number {
-        // Everything works right in development
-        if (this.$isDevelopment) {
-          return this.a + this.b;
-        }
-        // Everything breaks in production :D
-        else {
-          return this.a - this.b;
-        }
-      }
+    components: {
+      SearchInput
     }
   })
 </script>
 
 <style lang="scss">
-  h1 {
-    color: #35495e;
 
-    hr {
-      border-color: #41b883;
-    }
-  }
 </style>
