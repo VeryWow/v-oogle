d620d40b369d2e2360956236c9a142c39127ccd0
Karen <dev.kaskar2008@gmail.com>
2018-04-05 06:47:29 +0300

    Perefigachil

--- src/views/components/SearchInput.vue
+++ src/views/components/SearchInput.vue
@@ -1,9 +1,32 @@
 <template lang="pug">
-  form(@submit.prevent="submitLink")
+  form.search-input-component(
+  ref="frm"
+  onsubmit="return q.value!=''"
+  action="https://google.com/search")
     vue-suggest(
-      v-model="query"
-      :destyled="true"
-    )
+    v-model="query"
+    ref="suggestComponent"
+    value-attribute="id"
+    display-attribute="title"
+    spellcheck="false"
+    autocomplete="off"
+    name="q"
+    :debounce="100"
+    :list="getList"
+    :min-length="0"
+    :destyled="true"
+    @hide-list="onHideList"
+    @select="onSelect")
+      div(
+      slot="suggestion-item"
+      slot-scope="scope"
+      :title="scope.suggestion.description")
+        span(v-html="boldenSuggestion(scope)")
+
+      //- div(
+      //- slot="misc-item-below")
+      //-   button Google search
+
     button(type="submit" style="display:none")
 </template>
 
@@ -16,12 +39,135 @@
       VueSuggest
     },
     data() { return {
-      query: ''
+      query: '',
+      isSelected: false,
+      defaultList: [
+        {
+          id: 1,
+          title: 'vue-simple-suggest'
+        },
+        {
+          id: 2,
+          title: 'vue-simple-events'
+        },
+        {
+          id: 3,
+          title: 'vue-simple-headful'
+        }
+      ]
     }},
     methods: {
-      submitLink() {
-        location.replace('https://google.com/search?q=' + this['query']);
+      onSelect(item) {
+        if (item) {
+          this.isSelected = true
+        }
+      },
+      onHideList() {
+        console.log('onHideList')
+        if (this.isSelected) {
+          this.$refs.frm['submit']()
+          this.isSelected = false
+        }
+      },
+      boldenSuggestion({ suggestion, query }) {
+        let result = suggestion.title;
+        if (!query) return result;
+
+        const index = result.toLowerCase().indexOf(query.toLowerCase())
+
+        if (!~index) {
+          return result
+        }
+
+        const match = result.substr(query.length)
+        result = result.replace(match, match.bold())
+
+        return result;
+      },
+      getList (inputValue) {
+        return new Promise((resolve, reject) => {
+          if (!inputValue) {
+            resolve([...this.defaultList])
+            return
+          }
+
+          let url = `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&namespace=*&search=${inputValue}&limit=10&namespace=0&format=json`
+          fetch(url).then(response => {
+            if (!response.ok) {
+              reject()
+            }
+            response.json().then(json => {
+              let autocompleteText = json.shift()
+              autocompleteText = autocompleteText.trim()
+              let result: any[] = []
+              const fields = ['title', 'description', 'link']
+              json.forEach((part, i) => {
+                part.forEach((el, j) => {
+                  if (!result[j]) {
+                    result.push({
+                      id: j+1
+                    })
+                  }
+                  result[j][fields[i]] = el
+                })
+              })
+              resolve(result.length ? result : [...this.defaultList])
+              // resolve([...(json.items || [])])
+            }).catch(e => {
+              reject(e)
+            })
+          }).catch(error => {
+            reject(error)
+          })
+        })
       }
     }
   })
 </script>
+
+<style lang="scss">
+.search-input-component {
+  position: relative;
+  .input-wrapper {
+    position: relative;
+    z-index: 1;
+  }
+  input {
+    width: 100%;
+    border: none;
+    font-size: 16px;
+    line-height: 34px;
+    outline: none;
+
+    padding: 6px 9px 4px 16px;
+    background-color: #fff;
+    vertical-align: top;
+    border-radius: 2px;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08);
+    transition: box-shadow 200ms cubic-bezier(0.4, 0.0, 0.2, 1);
+
+    &:hover, &:focus {
+      box-shadow: 0 3px 8px 0 rgba(0,0,0,0.2), 0 0 0 1px rgba(0,0,0,0.08);
+    }
+  }
+
+  .suggestions {
+    background-color: #fff;
+    box-shadow: 0 3px 8px 0 rgba(0,0,0,0.2), 0 0 0 1px rgba(0,0,0,0.08);
+    border: none;
+    z-index: 2;
+    position: absolute;
+    right: 0;
+    left: 0;
+
+    .suggest-item {
+      padding: 0 16px;
+      line-height: 22px;
+
+      &:hover, &.hover {
+        background: #eee;
+      }
+    }
+  }
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,7 @@
 <template lang="pug">
 .v-oogle
   .logo-box
-    img(src="https://www.google.ru/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
-    //- img(
-    //-   alt="Google"
-    //-   height="92"
-    //-   src="https://www.google.ru/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-    //-   style="padding-top:109px"
-    //-   width="272")
+    img(alt="Google" src="https://www.google.ru/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
   .search-box
     search-input.search
 </template>
@@ -26,6 +20,7 @@
 <style lang="scss">
 * {
   box-sizing: border-box;
+  font-family: arial,sans-serif;
 }
 
 html {
@@ -49,25 +44,6 @@ html {
     .search {
       width: 484px;
       margin: 0 auto;
-
-      input {
-        width: 100%;
-        border: none;
-        font: 16px arial,sans-serif;
-        line-height: 34px;
-        outline: none;
-
-        padding: 6px 9px 4px 16px;
-        background-color: #fff;
-        vertical-align: top;
-        border-radius: 2px;
-        box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08);
-        transition: box-shadow 200ms cubic-bezier(0.4, 0.0, 0.2, 1);
-
-        &:hover, &:focus {
-          box-shadow: 0 3px 8px 0 rgba(0,0,0,0.2), 0 0 0 1px rgba(0,0,0,0.08);
-        }
-      }
     }
   }
 }
